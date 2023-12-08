03737f63ae45bf00e07a7dd9f87e38ed066b7009
KwonKieun <kgo6625@naver.com>
2023-12-08 17:14:13 +0900

    자바 1일차: 기본형 변수 선언 예제

---
+++ java_kke/src/day01/VariableEx3.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class VariableEx3 {
+
+	// 실수형 변수 선언 예제
+	public static void main(String[] args) {
+		//실수 리터럴 1.23의 타입은 double
+		float num1 = 1.23f;
+		System.out.println(num1);
+		
+	}		
+
+}
