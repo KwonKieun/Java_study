795471fbbc506d1df8d865dd1eed326e6e8941b2
KwonKieun <kgo6625@naver.com>
2023-12-08 17:40:59 +0900

    자바 1일차: 테스트

---
+++ java_kke/src/day01/TestEx2.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class TestEx2 {
+
+	public static void main(String[] args) {
+		// 2. 국어, 영어, 수학 성적을 입력하여 평균을 구하려 한다.
+		// 이 때 필요한 변수들을 선언하세요.
+			// 점수를 정수로 or 실수로?
+		// 정수
+		int kor1, eng1, math1, sum1;
+		double avg1;
+		// 실수
+		double kor2, eng2, math3, sum2, avg2;	
+	}
+}
