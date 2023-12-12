9e9dcbba9d6bdd71c66191c73afb996258c0cbd6
KwonKieun <kgo6625@naver.com>
2023-12-12 13:59:34 +0900

    자바 3일차 : switch문 예제 - 홀짝 판별

---
+++ java_kke/src/day03/SwitchEx2.java
@@ -0,0 +1,29 @@
+package day03;
+
+import java.util.Scanner;
+
+public class SwitchEx2 {
+
+	// switch문 예제
+	public static void main(String[] args) {
+		// 정수를 입력받아 입력받은 정수의
+		// 홀짝 판별 예제를 switch문을 이용하여 작성하시오.
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("정수를 입력하세요. : ");
+		int num = scan.nextInt();
+		
+		switch(num % 2) {
+		case 0:
+			System.out.println(num + "은 짝수입니다.");
+			break;
+		default:
+			System.out.println(num + "은 홀수입니다.");
+			break;
+		}
+		
+		scan.close();
+
+	}
+
+}
