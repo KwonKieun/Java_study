35df0d6bcabae8f9784d9c39e23ee3a32a358c23
KwonKieun <kgo6625@naver.com>
2024-02-27 14:43:54 +0900

    서블릿 4일차 : 서블릿 복습 - 게시글 등록 시 게시판 선택 적용 완료

--- servlet/servlet_project2/src/main/java/kr/kh/app/service/BoardServiceImp.java
+++ servlet/servlet_project2/src/main/java/kr/kh/app/service/BoardServiceImp.java
@@ -11,6 +11,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import kr.kh.app.dao.BoardDAO;
 import kr.kh.app.model.vo.BoardVO;
+import kr.kh.app.model.vo.CommunityVO;
 
 public class BoardServiceImp implements BoardService {
 
@@ -52,5 +53,10 @@ public class BoardServiceImp implements BoardService {
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public ArrayList<CommunityVO> getCommunityList() {
+		return boardDao.selectCommunityList();
 	}
 }
