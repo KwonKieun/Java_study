e8047f9a5aa46375d67bc9a4266a287441bff135
KwonKieun <kgo6625@naver.com>
2024-03-05 14:11:26 +0900

    서블릿 8일차 : 게시글 삭제 다중 첨부파일 구현 완료

--- servlet/servlet_project1/src/main/java/kr/kh/app/service/BoardServiceImp.java
+++ servlet/servlet_project1/src/main/java/kr/kh/app/service/BoardServiceImp.java
@@ -106,16 +106,18 @@ public class BoardServiceImp implements BoardService {
 		//게시글의 첨부파일을 서버 폴더에서 삭제(실제 파일)
 		//게시글의 첨부파일을 DB에서 삭제(CASCADE로 대체 가능)
 		//게시글에 있는 첨부파일 정보를 가져옴
-		//FileVO file = boardDao.selectFileByBo_num(num);
+		ArrayList<FileVO> fileList = boardDao.selectFileByBo_num(num);
 		
-		//deleteFile(file);
+		for(FileVO file : fileList) {
+			deleteFile(file);			
+		}
 
 		//같으면 게시글 삭제 후 삭제 여부를 반환
 		return boardDao.deleteBoard(num);
 	}
 
 	@Override
-	public boolean updateBoard(BoardVO board, MemberVO user, int fi_num, Part file) {
+	public boolean updateBoard(BoardVO board, MemberVO user, ArrayList<Integer> nums, ArrayList<Part> fileList) {
 		if(user == null || user.getMe_id() == null) {
 			return false;
 		}
@@ -132,11 +134,15 @@ public class BoardServiceImp implements BoardService {
 		}
 		
 		//첨부파일 추가
-		uploadFile(file, board.getBo_num());
+		for(Part file : fileList) {			
+			uploadFile(file, board.getBo_num());
+		}
 		
 		//첨부파일 삭제
-		FileVO fileVo = boardDao.selectFile(fi_num);
-		deleteFile(fileVo);
+		for(int fi_num : nums) {			
+			FileVO fileVo = boardDao.selectFile(fi_num);
+			deleteFile(fileVo);
+		}
 		
 		//같으면 게시글 수정
 		return boardDao.updateBoard(board);
