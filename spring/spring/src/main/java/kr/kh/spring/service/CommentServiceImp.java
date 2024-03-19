22a8d127be922861a1571df7fe35c50e6f9065b9
KwonKieun <kgo6625@naver.com>
2024-03-19 14:32:56 +0900

    스프링 5일차 : 댓글 수정 구현 완료

--- spring/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
+++ spring/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
@@ -63,4 +63,19 @@ public class CommentServiceImp implements CommentService {
 		//댓글 삭제
 		return commentDao.deleteComment(comment.getCm_num());
 	}
+
+	@Override
+	public boolean updateComment(CommentVO comment, MemberVO user) {
+		if(comment == null || !checkString(comment.getCm_content())) 
+			return false;
+		if(user == null)
+			return false;
+	
+		CommentVO dbComment = commentDao.selectComment(comment.getCm_num());
+		if(dbComment == null ||
+			!dbComment.getCm_me_id().equals(user.getMe_id()))
+			return false;
+		return commentDao.updateComment(comment);
+
+	}
 }
